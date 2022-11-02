b9c7e093ed0c7d121917845532a09583269a5f58
lupusalba <a145296@gmail.com>
2022-11-02 15:30:18 +0100

    basic redux strucutre

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// STORE -> GLOBALIZED STATE
+
+// ACTION INCREMENT
+const increment = () => {
+  return {
+    type : 'INCREMENT'
+  };
+};
+const decrement = () => {
+  return {
+    type : 'DECREMENT'
+  };
+};
+
+// REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state ;
+  }
+}
+
+const store = createStore(
+  counter, /* preloadedState, */
++  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// display in console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment());
+
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    
+      <App />
+
+  </React.StrictMode>,
+  document.getElementById('root')
+);
